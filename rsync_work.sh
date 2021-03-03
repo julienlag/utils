4b0060c1d8b07b5c895772d3b610bee36af49c2f
julien lagarde <julienlag@gmail.com>
2021-03-03 07:20:24 +0100

    version 20210303072014087658772

--- rsync_work.sh
+++ rsync_work.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
+source $HOME/.bashrc
 rsync -aqP --delete crg:/users/rg/jlagarde/projects/encode/scaling/whole_genome/lncRNACapture_phase3/plots $HOME/work/lncRNACapture_phase3/ &> $HOME/work/lncRNACapture_phase3.rsync.log &
-rsync -aqP --delete crg:/users/rg/jlagarde/projects/encode/scaling/whole_genome/lncRNACapture_phase3.1/plots $HOME/work/lncRNACapture_phase3.1/ &> $HOME/work/lncRNACapture_phase3.1.rsync.log &
