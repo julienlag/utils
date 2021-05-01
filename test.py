5725bd52c1d68c6cf8eff2e3791e9fa19eadb51e
julien lagarde <julienlag@gmail.com>
2021-05-01 07:20:12 +0200

    version 20210501072004733761594

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python
 
-import drmaa
+import pandas as pd
+from pprint import pprint
+sampleAnnot = pd.read_table(
+    "/users/project/gencode_006070/jlagarde/lncRNACapture_phase3/annotations/samples/sample_annotations.tsv", header=0, sep='\t')
+
+sampleAnnot.set_index('sample_name', inplace=True)
+
+# print(sampleAnnot)
+# sampleAnnot.astype({'reverse_transcriptase': str})
+sampleAnnotDict = sampleAnnot.to_dict('index')
+
+if sampleAnnotDict['pacBioSII:pacBio:IsoSeq:_HpreCap_0+_AlzhBrain']['libraryPrep'] == 'IsoSeq1':
+    pprint("Hello you guys")
+else:
+    pprint("Sorry you guys")
