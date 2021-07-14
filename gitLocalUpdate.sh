9d4aca6b781c1ecc00db606d4e6543ae69ef11d1
julien lagarde <julienlag@gmail.com>
2021-07-14 23:43:21 +0200

    version 20210714234318947755336

--- gitLocalUpdate.sh
+++ gitLocalUpdate.sh
@@ -13,7 +13,7 @@ git init
 
 echo -e "\n####\n## gitting $dir...\n####"
  for file in `find . -type f | grep -vP "~$" | grep -vP "\.git/"`; do
- git add $file;
+ git add -u $file;
 # add file to depository's index
  done;
 echo "Done gitting $dir..."
