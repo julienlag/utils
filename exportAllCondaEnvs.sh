47a7b5c7f0007e51110abf88868e4c8ca6bb0fad
julien lagarde <julienlag@gmail.com>
2021-07-29 07:20:13 +0200

    version 20210729072003686272384

--- exportAllCondaEnvs.sh
+++ exportAllCondaEnvs.sh
@@ -10,7 +10,7 @@ ENVS=$(conda env list | grep '^\w' | cut -d' ' -f1)
 for env in $ENVS; do
 	echo "Exporting $env..."
     conda activate $env
-    conda env export |grep -vP "^prefix: " > $EXPORT_DIR/$env.with-build.yml
+#    conda env export |grep -vP "^prefix: " > $EXPORT_DIR/$env.with-build.yml
     conda env export --no-builds |grep -vP "^prefix: " > $EXPORT_DIR/$env.yml
     echo "Done."
 done
